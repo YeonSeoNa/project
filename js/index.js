074f798384419db7361d9469a2edff538616ea45
YeonSeoNa <122263132+YeonSeoNa@users.noreply.github.com>
2023-02-07 17:23:08 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$('.slide_group').slick({
+    autoplay:true,       
+    autoplaySpeed:4000,
+    dots:true,         
+    arrows:true,       
+    prevArrow:'<button class="slick-prev slick-arrow"><i class="fa-solid fa-angle-left"></i></button>',
+    nextArrow:'<button class="slick-next slick-arrow"><i class="fa-solid fa-angle-right"></i></button>',
+})
