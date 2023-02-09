4eab36dc5797bcc724d0940b05f59b5cf0397a3e
YeonSeoNa <122263132+YeonSeoNa@users.noreply.github.com>
2023-02-09 13:24:48 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,10 +1,53 @@
 
 var ww = $(window).width()
-if (ww>992) {
+if (ww>1024) {
     $('html').addClass('pc')
 } else {
     $('html').addClass('mobile')
 }
+
+
+// 개발자도구창의 토글디바이스툴바 사용하지 않았을때 
+// 테스트 상황(윈도우 넓이 조절)에서 생기는 오류를 해결하기 위한 코드
+var deviceSize = 1024
+function scrollOX(status){
+    $('html').css({
+        overflowY:status
+    })
+    return $('html').width()
+}
+
+// 토글디바이스 툴바가 꺼있으면 스크롤바가 생성되므로
+// 스크롤바 넓이 17px을 deviceSize에서 빼야함
+var scX = scrollOX('hidden')
+var scO = scrollOX('scroll')
+var scD = scX - scO
+// 토글디바이스툴바가 켜져 있으면 아래 scX와 scO는 같은 값이 되므로
+// 아래 if문을 들어가지 않아서 deviceSize는 원래 값임
+if (scD>0) {
+    deviceSize = deviceSize - scD
+}
+
+
+function init(){
+    let ww = $(window).on('resize',function(){
+        let ww = $(this).width()
+        if(ww>deviceSize) {
+            $('html').addClass('pc').removeClass('mobile')
+        } else {
+            $('html').addClass('mobile').removeClass('pc')
+        }
+    })
+}
+init()
+
+$(window).on('resize',function(){
+    init()
+})
+
+
+
+
 
 $('#header .hamburger').on('click', function(){
     $(this).next().stop().slideToggle()
--- js/index.js
+++ js/index.js
@@ -6,3 +6,26 @@ $('.slide_group').slick({
     prevArrow:'<button class="slick-prev slick-arrow"><i class="fa-solid fa-angle-left"></i></button>',
     nextArrow:'<button class="slick-next slick-arrow"><i class="fa-solid fa-angle-right"></i></button>',
 })
+
+
+
+
+let article2Near = $('.article2').offset().top - $(window).height()/2
+let article3Near = $('.article3').offset().top - $(window).height()/2
+
+$(window).on('scroll', function(){
+    let sct = $(this).scrollTop()
+    
+    if (sct>=article2Near) {
+        $('.article2').addClass('on')
+    } else {
+        $('.article2').removeClass('on')
+    }
+
+    if (sct>=article3Near) {
+        $('.article3').addClass('on')
+    } else {
+        $('.article3').removeClass('on')
+    }
+
+})
