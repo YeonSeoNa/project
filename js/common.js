9640d8ff337b1b442c8cad4bea4f5b0e327ef687
YeonSeoNa <122263132+YeonSeoNa@users.noreply.github.com>
2023-02-14 10:24:20 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,14 +1,4 @@
 
-var ww = $(window).width()
-if (ww>1024) {
-    $('html').addClass('pc')
-} else {
-    $('html').addClass('mobile')
-}
-
-
-// 개발자도구창의 토글디바이스툴바 사용하지 않았을때 
-// 테스트 상황(윈도우 넓이 조절)에서 생기는 오류를 해결하기 위한 코드
 var deviceSize = 1024
 function scrollOX(status){
     $('html').css({
@@ -17,33 +7,32 @@ function scrollOX(status){
     return $('html').width()
 }
 
-// 토글디바이스 툴바가 꺼있으면 스크롤바가 생성되므로
-// 스크롤바 넓이 17px을 deviceSize에서 빼야함
 var scX = scrollOX('hidden')
 var scO = scrollOX('scroll')
 var scD = scX - scO
-// 토글디바이스툴바가 켜져 있으면 아래 scX와 scO는 같은 값이 되므로
-// 아래 if문을 들어가지 않아서 deviceSize는 원래 값임
+
 if (scD>0) {
     deviceSize = deviceSize - scD
 }
-
-
-function init(){
-    let ww = $(window).on('resize',function(){
-        let ww = $(this).width()
-        if(ww>deviceSize) {
-            $('html').addClass('pc').removeClass('mobile')
-        } else {
-            $('html').addClass('mobile').removeClass('pc')
-        }
-    })
+var ww = $(window).width()
+if (ww>deviceSize ) {
+    $('html').addClass('pc')
+} else {
+    $('html').addClass('mobile')
 }
-init()
 
-$(window).on('resize',function(){
-    init()
+
+$(window).on('resize', function(){
+    let ww = $(window).width()
+    if (ww>deviceSize && !$('html').hasClass('pc') ) {
+        $('html').addClass('pc').removeClass('mobile')
+        location.reload()
+    } else if (ww<=deviceSize && !$('html').hasClass('mobile')) {
+        $('html').addClass('mobile').removeClass('pc')
+        location.reload()
+    }
 })
+
 
 
 
@@ -70,8 +59,16 @@ $('#nav .depth1 > li > a').on('click', function(){
             $(this).find('i').removeClass('fa-chevron-up')
             .addClass('fa-chevron-down')
         }
+        $(this).parent().siblings().find('.depth2').slideUp()
+        $(this).parent().siblings().find('i')
+        .removeClass('fa-chevron-up')
+        .addClass('fa-chevron-down')
+        
         return false
     }
 })
+
+
+
 
 
--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 $('.slide_group').slick({
     autoplay:true,       
-    autoplaySpeed:4000,
+    autoplaySpeed:3000,
     dots:true,         
     arrows:true,       
     prevArrow:'<button class="slick-prev slick-arrow"><i class="fa-solid fa-angle-left"></i></button>',
     nextArrow:'<button class="slick-next slick-arrow"><i class="fa-solid fa-angle-right"></i></button>',
 })
+
 
 
 
